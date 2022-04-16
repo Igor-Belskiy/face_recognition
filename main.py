482eadfda88bc8cbdf6753b6be644d6b20779d75
Igor.Belskiy <igormobilemail@gmail.com>
2022-04-17 00:45:23 +0300

    face recognition on photo with girls

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-print('Hello World')
+import face_recognition
+from PIL import Image, ImageDraw
 
-print('second line')
+def face_rec():
+    gal_face_img = face_recognition.load_image_file('img/mnogo-devushek-belye.jpg')
+    gal_face_location = face_recognition.face_locations(gal_face_img)
 
-print('156')
+    print(gal_face_location)
+    print(f'Found {len(gal_face_location)} face(s) in this image')
 
-print('zero')
+    pil_img = Image.fromarray(gal_face_img)
+    draw1 = ImageDraw.Draw(pil_img)
 
-print('2')
+    for (top, right, bottom, left) in gal_face_location:
+        draw1.rectangle(((left, top),(right, bottom)), outline=(255, 255, 0), width=4)
+
+    del draw1
+    pil_img.save('img/mnogo-devushek-marked-faces.jpg')
+
+def main():
+    face_rec()
+
+if __name__ == '__main__':
+    main()
